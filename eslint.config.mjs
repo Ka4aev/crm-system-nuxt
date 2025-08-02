5012fa99b6a0fab045475d458b2921478c021290
Ka4aev <staskachaev205@mail.ru>
2025-08-02 19:15:53 +0700

    settings app

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+// @ts-nocheck
+import withNuxt from "./.nuxt/eslint.config.mjs";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+import * as eslintConfigPrettier from "eslint-config-prettier";
+
+export default withNuxt(
+  {
+    rules: {
+      "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "warn",
+    },
+  },
+  {
+    plugins: {
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      ...eslintConfigPrettier.rules,
+      "@typescript-eslint/no-explicit-any": "off",
+      "vue/multi-word-component-names": "off",
+      "prettier/prettier": ["warn", { endOfLine: "auto" }],
+    },
+  }
+);
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/globals.css"],
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/eslint",
+    "shadcn-nuxt",
+  ],
+  shadcn: {
+    prefix: "Ui",
+    componentDir: "./app/components/ui",
+  },
+  vite: {
+    resolve: {
+      alias: {
+        "~/lib/utils": "./app/utils/shadcn/utils"
+      }
+    }
+  }
+});
